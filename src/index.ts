367e4cbb0d2cd616543c24a73464fdbf4705c019
Sam Scott <samoscarscott@gmail.com>
2022-12-22 15:37:45 +0000

    add first test

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ const example = () => {
   console.log("hello");
 };
 
+export const equal = (a: number, b: number) => {
+  return a === b;
+};
+
 export default example;
--- test/default.test.ts
+++ test/default.test.ts
@@ -1,9 +1,16 @@
-import { describe, test } from "vitest";
-
-import field from "../src";
+import field, { equal } from "../src";
+import { describe, test, expect } from "vitest";
 
 describe("Default Tests", () => {
   test("It works", () => {
     field();
+  });
+});
+
+describe("Test", () => {
+  test("add", () => {
+    expect(equal(5, 5)).toBe(true);
+    expect(equal(5, 1)).toBe(false);
+    expect(equal(5, 4)).toBe(false);
   });
 });
